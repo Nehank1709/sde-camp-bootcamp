fe793eea323c0d4008be05518cfd7c55548faa99
Neha Kumari <55323347+Nehank1709@users.noreply.github.com>
2022-02-17 23:54:01 +0530

    Create Subsets.java

---
+++ Day 6/Subsets.java
@@ -0,0 +1,30 @@
+import java.util.*; 
+public class Subsets
+{
+    static List<List<Integer>> res;
+    public static void subsets(int[] nums,int index,List<Integer> list){
+        if(index==nums.length-1){
+            res.add(new ArrayList<Integer>(list));
+            list.add(nums[index]);
+            res.add(new ArrayList<Integer>(list));
+            list.remove(list.size()-1);
+            return;
+        }
+        subsets(nums,index+1,list);
+        list.add(nums[index]);
+        subsets(nums,index+1,list);
+        list.remove(list.size()-1);
+    }
+  public static void main(String[] args) {
+      res = new ArrayList<>();
+      int[] nums={2, 3, 4};
+      List<Integer> list = new ArrayList<Integer>();
+      subsets(nums, 0, list);
+      for(List<Integer> subset:res){
+          for(int i: subset){
+              System.out.print(i+", ");
+          }
+            System.out.println();
+      }
+  }
+}
