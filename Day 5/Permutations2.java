4e5d84933237abbad855e050aadece3d94bbdf0b
Neha Kumari <55323347+Nehank1709@users.noreply.github.com>
2022-02-17 23:51:03 +0530

    Create Permutations2.java

---
+++ Day 5/Permutations2.java
@@ -0,0 +1,31 @@
+public class Permutations2 {
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+        if(num==null||num.length<=0) return result;
+        Arrays.sort(num);
+        ArrayList<Integer> res=new ArrayList<Integer>();
+        boolean[] used=new boolean[num.length];
+        for(int i=0;i<used.length;i++) used[i]=false;
+        dfs(result,res,num,used,num.length);
+        return result;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> result,ArrayList<Integer> res,int[] num,boolean[] used,int n){
+        if(n==0){
+            ArrayList<Integer> cur=new ArrayList<Integer>();
+            for(int i=0;i<res.size();i++) cur.add(res.get(i));
+            result.add(cur);
+            return;
+        }
+        for(int i=0;i<used.length;i++){
+            if(used[i]==true||(i!=0&&num[i]==num[i-1]&&used[i-1]==true))
+                continue;
+            used[i]=true;
+            res.add(num[i]);
+            dfs(result,res,num,used,n-1);
+            res.remove(res.size()-1);
+            used[i]=false;
+        }
+    }
+}
